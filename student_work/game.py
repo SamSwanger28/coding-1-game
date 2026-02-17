0e711daa1d6247638e3c84440f17c0c62cdb486c
Sam Swanger <samuel.e.swanger@gmail.com>
2026-02-17 20:29:48 +0000

    did a little more planning and added some more info to the class and made the enemy class

--- student_work/game.py
+++ student_work/game.py
@@ -4,9 +4,15 @@ class Game():
         self.game_data = {
     # Store board dimensions, player/enemy positions, score, energy, collectibles, and icons
 }
+        self.player_data = {    
+}
     def draw_board(self,screen):
     # Print the board and all game elements using curses
         pass
 
+class Enemy():
+    def __init__(self):
+        self.enemy_data = {
+}
 
 # Good Luck!
