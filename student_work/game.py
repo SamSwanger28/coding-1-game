043a227e90cef42fc81a73a567e699f6d87951e5
Sam Swanger <samuel.e.swanger@gmail.com>
2026-02-18 16:57:26 +0000

    imported useful libraries and et up the unicode for assets

--- student_work/game.py
+++ student_work/game.py
@@ -1,14 +1,37 @@
 # Write your game here
+import curses
+import random
+import time
+
 class Game():
     def __init__(self):
         self.game_data = {
     # Store board dimensions, player/enemy positions, score, energy, collectibles, and icons
+    'Board_Width' : 10,
+    'Board_Height' : 10,
+    'Obstacle' : [ {'x' : random.randint(10), 'y' : random.randint(10)},
+
+    ],
+
+    # Board pngs
+    'obstacle': "\U0001FAA8 ",# 🪨
+    'sword' : "\U0001f5e1\ufe0f", # 🗡️
+    'axe' : "\U0001fa93", # 🪓
+    'bow' : "\U0001f3f9", # 🏹
+    'empty': "  "
 }
-        self.player_data = {    
-}
-    def draw_board(self,screen):
+        self.player_data = {       
+            'Player_Start' : (5,5),
+            'Player_Health' : 5,
+ }
+    
+    def draw_board(self,stdsrc):
     # Print the board and all game elements using curses
-        pass
+        stdsrc.clear
+        for y in range(self.game_data["Board_Height"]):
+            row = " "
+            for x in range(self.game_data["Board_Width"]):
+                pass
 
 class Enemy():
     def __init__(self):
